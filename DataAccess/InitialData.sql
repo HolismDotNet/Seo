86d8c62fb330e1dba9c44dcc1f866a7668e88263
Saeed Nemati <sn.1361@gmail.com>
2022-02-05 22:46:46 +0330

    committed by script

--- DataAccess/InitialData.sql
+++ DataAccess/InitialData.sql
@@ -7,53 +7,88 @@ from NodeTypes
 where `Key` = 'JsonArray';
 
 select Id into @textId
+from NodeTypes
 where `Key` = 'text';
 
-select *
-from Nodes 
-where `Key` = 'FAQPage';
-
-if not found_rows() then
-    insert into `Nodes` (`Key`, `ParentId`, `NodeTypeId`, `Value`) values
-    ('FAQPage',	null,	@jsonObjectId,	null)
-end if;
+insert into Nodes (`Key`, ParentId, NodeTypeId)
+select 'FAQPage', null, 1
+where not exists
+(
+    select null
+    from Nodes 
+    where `Key` <=> 'FAQPage'
+    and ParentId <=> null
+);
 
 select Id into @faqpageId
 from Nodes
 where `Key` = 'FAQPage';
 
 insert ignore into `Nodes` (`Key`, `ParentId`, `NodeTypeId`, `Value`) values
-('@context',	@faqpageId,	@textId,	'https://schema.org'),
-('@type',	@faqpageId,	@textId,	'FAQPage'),
-('mainEntity',	@faqpageId,	@jsonArrayId,	null);
+('@context', @faqpageId, @textId, 'https://schema.org'),
+('@type', @faqpageId, @textId, 'FAQPage'),
+('mainEntity', @faqpageId, @jsonArrayId, null);
 
 select Id into @mainEntityId
 from Nodes 
-where `Key` = 'mainEntity'
+where `Key` = 'mainEntity';
 
 insert ignore into `Nodes` (`Key`, `ParentId`, `NodeTypeId`, `Value`) values
-('Question',	(select Id from Nodes where `Key` = 'mainEntity'),	1,	null);
+('Question', @mainEntityId, 1, null);
+
+select Id into @questionId
+from Nodes
+where `Key` = 'Question';
 
 insert ignore into `Nodes` (`Key`, `ParentId`, `NodeTypeId`, `Value`) values
-('@type',	(select Id from Nodes where `Key` = 'Question'),	@textId,	'Question'),
-('name',	(select Id from Nodes where `Key` = 'Question'),	@textId,	null),
-('acceptedAnswer',	select Id from Nodes where `Key` = 'Question'),	1,	null);
+('@type', @questionId, @textId, 'Question'),
+('name', @questionId, @textId, null),
+('acceptedAnswer', @questionId, 1, null);
+
+select Id into @acceptedAnswerId
+from Nodes
+where `Key` = 'acceptedAnswer';
 
 insert ignore into `Nodes` (`Key`, `ParentId`, `NodeTypeId`, `Value`) values
-('@type',	(select Id from Nodes where `Key` = 'acceptedAnswer'),	@textId,	'Answer'),
-('text',	(select Id from Nodes where `Key` = 'acceptedAnswer'),	@textId,	null);
+('@type', @acceptedAnswerId, @textId, 'Answer'),
+('text', @acceptedAnswerId, @textId, null);
 
-insert ignore into `Nodes` (`Key`, `ParentId`, `NodeTypeId`, `Value`) values
-('Product',	null,	1,	null),
-('@context',	(select Id from Nodes where `Key` = 'Product'),	@textId,	'https://schema.org/'),
-('@type',	(select Id from Nodes where `Key` = 'Product'),	@textId,	'Product'),
-('name',	(select Id from Nodes where `Key` = 'Product'),	@textId,	null),
-('image',	(select Id from Nodes where `Key` = 'Product'),	@jsonArrayId,	null),
-('description',	(select Id from Nodes where `Key` = 'Product'),	@textId,	null),
-('brand',	(select Id from Nodes where `Key` = 'Product'),	1,	null),
-('@type',	(select Id from Nodes where `Key` = 'Brand'),	@textId,	'Brand'),
-('name',	(select Id from Nodes where `Key` = 'Brand'),	@textId,	null),
-('aggregateRating',	(select Id from Nodes where `Key` = 'Product'),	1,	null),
-('@type',	(select Id from Nodes where `Key` = 'aggregateRating'),	@textId,	'AggregateRating'),
-('ratingValue',	(select Id from Nodes where `Key` = 'aggregateRating'),	@textId,	null),
-('reviewCount',	(select Id from Nodes where `Key` = 'aggregateRating'),	@textId,	null);
+insert into Nodes (`Key`, ParentId, NodeTypeId)
+select 'Product', null, 1
+where not exists
+(
+    select null
+    from Nodes 
+    where `Key` <=> 'Product'
+    and ParentId <=> null
+);
+
+select Id into @productId
+from Nodes 
+where `Key` = 'Product';
+
+insert ignore into Nodes (`Key`, ParentId, NodeTypeId, `Value`) values
+('@context', @productId, @textId, 'https://schema.org/'),
+('@type', @productId, @textId, 'Product'),
+('name', @productId, @textId, null),
+('image', @productId, @jsonArrayId, null),
+('description', @productId, @textId, null),
+('brand', @productId, 1, null);
+
+select Id into @brandId
+from Nodes
+where `Key` = 'brand';
+
+insert ignore into Nodes (`Key`, ParentId, NodeTypeId, `Value`) values
+('@type', @brandId, @textId, 'Brand'),
+('name', @brandId, @textId, null),
+('aggregateRating', @productId, 1, null);
+
+select Id into @aggregateRagingId
+from Nodes
+where `Key` = 'aggregateRating';
+
+insert ignore into Nodes (`Key`, ParentId, NodeTypeId, `Value`) values
+('@type', @aggregateRagingId, @textId, 'AggregateRating'),
+('ratingValue', @aggregateRagingId, @textId, null),
+('reviewCount', @aggregateRagingId, @textId, null);
