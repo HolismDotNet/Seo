458f44dfc71b4e488f2efb644892e3238f9137fe
Saeed Nemati <sn.1361@gmail.com>
2022-02-12 19:27:37 +0330

    committed by script

--- Api/Admin/SnippetNodeController.cs
+++ Api/Admin/SnippetNodeController.cs
@@ -2,5 +2,8 @@ namespace Seo;
 
 public class SnippetNodeController : TreeController<NodeView, Node>
 {
+    public override ReadBusiness<NodeView> ReadBusiness => new NodeBusiness();
+
     public override TreeBusiness<NodeView, Node> Business => new NodeBusiness();
+
 }
