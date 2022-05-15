b06e1fe81534d407bfb8a7ca9247ee73fa63c32e
Saeed Nemati <sn.1361@gmail.com>
2022-05-15 16:22:39 +0430

    committed by script

--- DataAccess/Repository.cs
+++ DataAccess/Repository.cs
@@ -26,22 +26,6 @@ public class Repository
         }
     }
 
-    public static Write<Seo.Node> Node
-    {
-        get
-        {
-            return new Write<Seo.Node>(new SeoContext());
-        }
-    }
-
-    public static Write<Seo.NodeView> NodeView
-    {
-        get
-        {
-            return new Write<Seo.NodeView>(new SeoContext());
-        }
-    }
-
     public static Write<Seo.ParametersObject> ParametersObject
     {
         get
@@ -55,22 +39,6 @@ public class Repository
         get
         {
             return new Write<Seo.QueryParameter>(new SeoContext());
-        }
-    }
-
-    public static Write<Seo.Snippet> Snippet
-    {
-        get
-        {
-            return new Write<Seo.Snippet>(new SeoContext());
-        }
-    }
-
-    public static Write<Seo.SnippetValue> SnippetValue
-    {
-        get
-        {
-            return new Write<Seo.SnippetValue>(new SeoContext());
         }
     }
 }
--- Models/NodeType.cs
+++
@@ -1,9 +0,0 @@
-namespace Seo;
-
-public enum NodeType
-{
-    Unknwon = 0,
-    JsonObject = 1,
-    JsonArray = 2,
-    Text = 3
-}
