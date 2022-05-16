c7dd0160aa5991eddaf6dbbc478a4b91b9afffbe
Saeed Nemati <sn.1361@gmail.com>
2022-05-16 10:12:27 +0430

    committed by script

--- Api/Admin/SnippetController.cs
+++
@@ -1,6 +0,0 @@
-namespace Seo;
-
-public class SnippetController : ReadController<Snippet>
-{
-    public override ReadBusiness<Snippet> ReadBusiness => new SnippetBusiness();
-}
--- Business/NodeBusiness.cs
+++
@@ -1,8 +0,0 @@
-namespace Seo;
-
-public class NodeBusiness : TreeBusiness<NodeView, Node>
-{
-    protected override Read<NodeView> Read => Repository.NodeView;
-
-    protected override Write<Node> Write => Repository.Node;
-}
